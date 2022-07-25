94fb264061d0c4d7e90ad71875512bda035e95d6
Esmurzelly <78688219+Esmurzelly@users.noreply.github.com>
2022-07-26 00:04:12 +0300

    burger_menu

---
+++ script.js
@@ -0,0 +1,29 @@
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu_list").cloneNode(1);
+const body = document.body;
+const links = Array.from(menu.children);
+
+
+hamb.addEventListener("click", hambHandler);
+links.forEach((link) => {
+    link.addEventListener("click", closeOnClick)
+})
+
+function hambHandler (e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
+}
+
+function closeOnClick() {
+    popup.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("noscroll");
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
